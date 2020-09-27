6eabbfd822c14c9290e0cc7d2f57ae28886f91f3
Ajinkya Gangawane <ajinkya.gangawane@pegasusinfocorp.com>
2020-09-28 02:23:12 +0530

    Sarafu first Commit

--- jmeterExecuteShellFile.sh
+++ jmeterExecuteShellFile.sh
@@ -1,14 +1,1 @@
-sh jmeter -n \
-	-t= SarafuApi.jmx\
-	-j= results.log\
-	-l= results.xml\
-	-Jjmeter.sava.saveservice.print_field_names=true \
-	-Jjmeter.save.saveservice.output_format=xml \
-	-Jjmeter.save.saveservice.response_data=true \
-	-Jjmeter.save.saveservice.samplerData=true \
-	-JnumberOfRequestPerClient=1 \
-	-Jusername="+255-710100099" \
-	-Jpassword="12345" \
-	-JnewPassword="1234" \
-	-ISO="TZA" 
-	
+sh jmeter -n -t SarafuApi.jmx -j results.log -l results.xml -Jjmeter.sava.saveservice.print_field_names=true -Jjmeter.save.saveservice.output_format=xml -Jjmeter.save.saveservice.response_data=true -Jjmeter.save.saveservice.samplerData=true -JnumberOfRequestPerClient=1 -Jusername="+255-710100099" -Jpassword="12345" -JnewPassword="1234" -JISO="TZA"
